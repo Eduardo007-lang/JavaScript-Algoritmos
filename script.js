5a8d5900b2054375b380eb3152b4f749396ca57b
unknown <eduardo2018ads@hotmail.com>
2022-12-14 11:01:24 -0300

    Script

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 
+// let count = 0;
+// while (count <= 10){
+//     console.log(count);
+//     count ++;
+// }
 
+// loop For
+// for(let count = 0; count <= 12; count++) {
 
+//     console.log(count)
+// }
 
+for (let number = 20; ; number++){
+    if(number % 7 == 0) {
+        break;
+    }
+}
 
-
-
-
-
+console.log(number);
 
